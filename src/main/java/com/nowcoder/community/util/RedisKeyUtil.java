7fd8e754ae0bb480e8160684250d75cd7b797b63
huahua123 <csstudy23@outlook.com>
2024-12-18 00:19:38 +0800

    第23次提交（20241217） 新增功能：优化登录模块

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -7,6 +7,9 @@ public class RedisKeyUtil {
     private static final String PREFIX_USER_LIKE = "like:user";
     private static final String PREFIX_FOLLOWEE = "followee";
     private static final String PREFIX_FOLLOWER = "follower";
+    private static final String PREFIX_KAPTCHA = "kaptcha";
+    private static final String PREFIX_TICKET = "ticket";
+    private static final String PREFIX_USER = "user";
 
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
@@ -30,6 +33,21 @@ public class RedisKeyUtil {
     // follower:entityType:entityId -> zset(userId,now)
     public static String getFollowerKey(int entityType, int entityId) {
         return PREFIX_FOLLOWER + SPILIT + entityType + SPILIT + entityId;
+    }
+
+    // 登录验证码
+    public static String getKaptchaKey(String owner) {
+        return PREFIX_KAPTCHA + SPILIT + owner;
+    }
+
+    // 登录的凭证
+    public static String getTicketKey(String ticket) {
+        return PREFIX_TICKET + SPILIT + ticket;
+    }
+
+    // 用户
+    public static String getUserKey(int userId) {
+        return PREFIX_USER + SPILIT + userId;
     }
 
 
